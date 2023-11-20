75d44a7f4b9b43ad2f862b5fd7460f69dfe1eeaf
CyanSpaghetti <69961623+CyanSpaghetti@users.noreply.github.com>
2023-11-20 19:42:55 +0500

    Update game.py
    Added some comments

--- src/game.py
+++ src/game.py
@@ -4,20 +4,20 @@ import sys, os
 from src.settings import window
 
 class Game:
-    def __init__(self):
+    def __init__(self): # object constructor for the game
         self.running = True
         self.window = py.display.set_mode((window.width,window.height))
-    def init(self):
+    def init(self): # initialize settings
         py.display.set_caption(window.title)
-    def update(self):
+    def update(self): # update each frame
         pass    
-    def input(self):
+    def input(self): # take input 
         for event in py.event.get():
             if event.type == py.QUIT:
                 self.running = False
-    def draw(self):
+    def draw(self): #draw graphics 
         pass
-    def quit(self):
+    def quit(self): # quit game 
         py.quit()
         sys.exit()
 
